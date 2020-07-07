a4390815b094ff7399616e882c2e1924ad4a02d3
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2020-07-07 20:04:35 -0300

    Arquivos da SOlução
    Ferramentas para incorporar a solução.

---
+++ frmPingMaps.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PingMaps
+{
+    partial class frmPingMaps
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.lblResultado1 = new System.Windows.Forms.Label();
+            this.lblResultado2 = new System.Windows.Forms.Label();
+            this.lblLogradouro = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.wbMaps = new System.Windows.Forms.WebBrowser();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(490, 4);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(156, 23);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblURL.Location = new System.Drawing.Point(12, 9);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(58, 13);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "IP / URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(92, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(392, 20);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.Validated += new System.EventHandler(this.txtURL_Validated);
+            // 
+            // lblResultado1
+            // 
+            this.lblResultado1.AutoSize = true;
+            this.lblResultado1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado1.Location = new System.Drawing.Point(12, 49);
+            this.lblResultado1.Name = "lblResultado1";
+            this.lblResultado1.Size = new System.Drawing.Size(0, 16);
+            this.lblResultado1.TabIndex = 3;
+            // 
+            // lblResultado2
+            // 
+            this.lblResultado2.AutoSize = true;
+            this.lblResultado2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado2.Location = new System.Drawing.Point(12, 80);
+            this.lblResultado2.Name = "lblResultado2";
+            this.lblResultado2.Size = new System.Drawing.Size(0, 16);
+            this.lblResultado2.TabIndex = 4;
+            // 
+            // lblLogradouro
+            // 
+            this.lblLogradouro.AutoSize = true;
+            this.lblLogradouro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogradouro.Location = new System.Drawing.Point(13, 170);
+            this.lblLogradouro.Name = "lblLogradouro";
+            this.lblLogradouro.Size = new System.Drawing.Size(71, 13);
+            this.lblLogradouro.TabIndex = 5;
+            this.lblLogradouro.Text = "Logradouro";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(90, 167);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.Size = new System.Drawing.Size(234, 20);
+            this.txtLogradouro.TabIndex = 6;
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Location = new System.Drawing.Point(72, 321);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(100, 23);
+            this.btnLocalizar.TabIndex = 7;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // wbMaps
+            // 
+            this.wbMaps.Location = new System.Drawing.Point(340, 137);
+            this.wbMaps.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMaps.Name = "wbMaps";
+            this.wbMaps.Size = new System.Drawing.Size(642, 400);
+            this.wbMaps.TabIndex = 8;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(12, 207);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(46, 13);
+            this.lblCidade.TabIndex = 9;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(12, 247);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(46, 13);
+            this.lblEstado.TabIndex = 10;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCEP.Location = new System.Drawing.Point(13, 291);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(31, 13);
+            this.lblCEP.TabIndex = 11;
+            this.lblCEP.Text = "CEP";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(72, 204);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(252, 20);
+            this.txtCidade.TabIndex = 12;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(72, 244);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(100, 20);
+            this.txtEstado.TabIndex = 13;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(72, 284);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 14;
+            // 
+            // frmPingMaps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 549);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.lblCEP);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.wbMaps);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.lblLogradouro);
+            this.Controls.Add(this.lblResultado2);
+            this.Controls.Add(this.lblResultado1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "frmPingMaps";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping / Maps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label lblResultado1;
+        private System.Windows.Forms.Label lblResultado2;
+        private System.Windows.Forms.Label lblLogradouro;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.WebBrowser wbMaps;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtCEP;
+    }
+}
+
---
+++ frmPingMaps.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace PingMaps
+{
+    public partial class frmPingMaps : Form
+    {
+        public frmPingMaps()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            Ping p = new Ping();
+            PingReply r;
+            string s;
+            s = txtURL.Text;
+            r = p.Send(s);
+            if (r.Status == IPStatus.Success)
+            {
+                lblResultado1.Text = "Ping para " + s.ToString() + "[" + r.Address.ToString() + "]" + " Sucesso!";
+                lblResultado2.Text = "Delay da resposta = " + r.RoundtripTime.ToString() + "ms" + "\n";
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível encontrar uma máquina com este endereço!", "PING", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void txtURL_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtURL.Text) || txtURL.Text == "")
+            {
+                MessageBox.Show("Digite um IP ou URL válida!", "PING", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string street = string.Empty;
+                string city = string.Empty;
+                string state = string.Empty;
+                string zip = string.Empty;
+
+                StringBuilder queryAddress = new StringBuilder();
+                queryAddress.Append("http://maps.google.com/maps?q=");
+
+                if (txtLogradouro.Text !=string.Empty)
+                {
+                    street = txtLogradouro.Text.Replace(' ', '+');
+                    queryAddress.Append(street + ',' + '+');
+                }
+
+                if (txtCidade.Text != string.Empty)
+                {
+                    city = txtCidade.Text.Replace(' ', '+');
+                    queryAddress.Append(city + ',' + '+');
+                }
+
+                if (txtEstado.Text != string.Empty)
+                {
+                    state = txtEstado.Text.Replace(' ', '+');
+                    queryAddress.Append(state + ',' + '+');
+                }
+
+                if (txtCEP.Text != string.Empty)
+                {
+                    zip = txtCEP.Text.Replace(' ', '+');
+                    queryAddress.Append(zip + ',' + '+');
+                }
+
+                wbMaps.Navigate(queryAddress.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Incapaz de retornar o mapa!");
+            }
+        }
+    }
+}
